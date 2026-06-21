73667e5a8c1280073718275400ed76d3021848bb
Yuri Liubuskyi <liubuskiy.yuri@gmail.com>
2026-06-21 18:09:27 +0300

    Connecting to Postgres

--- server/index.js
+++ server/index.js
@@ -1,2 +1,47 @@
+const keys = require("./keys");
 const express = require("express");
 const cors = require("cors");
+const { Pool } = require("pg");
+const redis = require("redis");
+
+// Express app setup
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// PG Client setup
+const pgClient = new Pool({
+  host: keys.pgHost,
+  port: keys.pgPort,
+  user: keys.pgUser,
+  password: keys.pgPassword,
+  database: keys.pgDatabase,
+});
+
+pgClient.on("error", () => console.log("Lost PG connection"));
+
+pgClient.query("CREATE TABLE IF NOT EXISTS values (number INT)", (err, res) => {
+  if (err) console.log(err);
+  else console.log("Table created");
+});
+
+// Redis setup
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000,
+});
+const redisPublisher = redisClient.duplicate();
+
+// Express routes
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/values", async (req, res) => {
+  const index = req.body.index;
+  redisPublisher.publish("insert", index);
+  pgClient.query("INSERT INTO values(number) VALUES($1)", [index]);
+  res.send("Success");
+});
